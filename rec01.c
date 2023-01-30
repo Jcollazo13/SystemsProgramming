f094cddcdbd0fed780bf687a83b835eed4b8bbd5
Jcollazo13 <91155408+Jcollazo13@users.noreply.github.com>
2023-01-29 20:59:00 -0600

    Creating file w/ initial code

---
+++ rec01.c
@@ -0,0 +1,56 @@
+// Jonathan Collazo || EUID: 11535014 || CSCE 3600 Systems Programming
+
+#include <stdio.h>
+
+
+char getUserInput()
+{
+    unsigned char target;
+    printf("Enter a printable ASCII character: "); 
+    scanf("%c", &target);
+    printf("%c", target);
+    return target;
+
+}
+
+
+void printBase10(char target)
+{
+    printf("\nBase 10: %d", target);
+
+}
+
+void printHex(char target)
+{
+    printf("\nHexadecimal: %x", target);
+}
+
+void printBinary(char target)
+{
+    int temp; 
+    printf("\nBinary: ");
+    for(int i = 7; i >= 0; i--)
+    {
+       temp = target >> i; 
+       if(temp & 1 == 1)
+       {
+        printf("1");
+       }
+       else 
+       {
+        printf("0");
+       }
+    }
+    printf("\n");
+
+}
+
+int main()
+{
+    unsigned char userValue = getUserInput();
+    printBase10(userValue);
+    printHex(userValue);
+    printBinary(userValue);
+    return 0;
+    
+}
